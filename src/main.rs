1f7c0fc71adba8eb9ea6e809b6b2af3d741842af
Andrew Marquez <andy2mgcc@gmail.com>
2022-12-01 00:03:15 -0700

    Run the solution for each day in main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use std::process::Command;
+
 fn main() {
-    println!("Hello, world!");
+    println!();
+    println!("🎄 Advent of Code 🎄");
+    println!();
+
+    for day in 1..=2 {
+        let day = format!("{:02}", day);
+        let cmd = Command::new("cargo")
+            .args(["run", "--bin", &day])
+            .output()
+            .unwrap();
+
+        println!("Day {}", day);
+        let output = String::from_utf8(cmd.stdout).unwrap();
+        if output.is_empty() {
+            println!("Not implemented")
+        } else {
+            println!("{}", output.trim());
+        }
+        println!();
+    }
 }
