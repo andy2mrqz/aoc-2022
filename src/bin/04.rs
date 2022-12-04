60d0c354ad01d2405161f17717f2227977ed2eda
Andrew Marquez <andy2mgcc@gmail.com>
2022-12-03 23:27:12 -0700

    Use HashSet for is_subset and !is_disjoint
    * This simplifies the redundant logic and makes it easier to read,
      although unsure whether the overhead of constructing the HashSet beats
      out the time complexity of calling Range.contains many times

--- src/bin/04.rs
+++ src/bin/04.rs
@@ -1,37 +1,33 @@
-use std::ops::RangeInclusive;
+use std::collections::HashSet;
 
-type Range = RangeInclusive<u32>;
+type Set = HashSet<u32>;
 
-fn overlap_count_where(input: &str, overlap_fn: &dyn Fn(&(Range, Range)) -> bool) -> usize {
+fn overlap_count_where(input: &str, overlap_fn: &dyn Fn(&Set, &Set) -> bool) -> usize {
     input
         .lines()
         .map(|assignments| {
             let assignments: Vec<&str> = assignments.split(",").collect();
 
             let left_range: Vec<u32> = assignments[0].split("-").flat_map(|n| n.parse()).collect();
-            let left_range = left_range[0]..=left_range[1];
+            let left_set: HashSet<u32> = (left_range[0]..=left_range[1]).collect();
 
             let right_range: Vec<u32> = assignments[1].split("-").flat_map(|n| n.parse()).collect();
-            let right_range = right_range[0]..=right_range[1];
+            let right_set: HashSet<u32> = (right_range[0]..=right_range[1]).collect();
 
-            (left_range, right_range)
+            (left_set, right_set)
         })
-        .filter(overlap_fn)
+        .filter(|(left_set, right_set)| {
+            overlap_fn(left_set, right_set) || overlap_fn(right_set, left_set)
+        })
         .count()
 }
 
 fn part_one(input: &str) -> usize {
-    overlap_count_where(input, &|(left_range, right_range)| {
-        left_range.contains(right_range.start()) && left_range.contains(right_range.end())
-            || right_range.contains(left_range.start()) && right_range.contains(left_range.end())
-    })
+    overlap_count_where(input, &|left_set, right_set| left_set.is_subset(&right_set))
 }
 fn part_two(input: &str) -> usize {
-    overlap_count_where(input, &|(left_range, right_range)| {
-        left_range.contains(right_range.start())
-            || left_range.contains(right_range.end())
-            || right_range.contains(left_range.start())
-            || right_range.contains(left_range.end())
+    overlap_count_where(input, &|left_set, right_set| {
+        !left_set.is_disjoint(right_set)
     })
 }
 
