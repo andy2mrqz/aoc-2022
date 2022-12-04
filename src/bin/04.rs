11602445a67ae7b24df9abe0ce4d209c3391875e
Andrew Marquez <andy2mgcc@gmail.com>
2022-12-03 22:28:28 -0700

    Solve day 04 part 2 (plus cleanup)

--- src/bin/04.rs
+++ src/bin/04.rs
@@ -1,39 +1,47 @@
-fn parse_input(input: &str) -> Vec<bool> {
+use std::ops::RangeInclusive;
+
+fn parse_input(input: &str) -> Vec<(RangeInclusive<u32>, RangeInclusive<u32>)> {
     input
         .lines()
         .map(|assignments| {
             let assignments: Vec<&str> = assignments.split(",").collect();
-            let [left, right]: [&str; 2] = assignments.try_into().unwrap();
 
-            let left_range: Vec<u32> = left.split("-").flat_map(|n| n.parse()).collect();
-            let [left_start, left_end]: [u32; 2] = left_range.try_into().unwrap();
-            let left_range = left_start..=left_end;
+            let left_range: Vec<u32> = assignments[0].split("-").flat_map(|n| n.parse()).collect();
+            let left_range = left_range[0]..=left_range[1];
 
-            let right_range: Vec<u32> = right.split("-").flat_map(|n| n.parse()).collect();
-            let [right_start, right_end]: [u32; 2] = right_range.try_into().unwrap();
-            let right_range = right_start..=right_end;
+            let right_range: Vec<u32> = assignments[1].split("-").flat_map(|n| n.parse()).collect();
+            let right_range = right_range[0]..=right_range[1];
 
-
-            left_range.contains(right_range.start()) &&
-            left_range.contains(right_range.end()) || 
-            right_range.contains(left_range.start()) &&
-            right_range.contains(left_range.end())
+            (left_range, right_range)
         })
         .collect()
 }
 
 fn part_one(input: &str) -> usize {
-    parse_input(input).iter().filter(|n| *n.to_owned() == true).count()
+    parse_input(input)
+        .iter()
+        .filter(|(left_range, right_range)| {
+            left_range.contains(right_range.start()) && left_range.contains(right_range.end())
+                || right_range.contains(left_range.start())
+                    && right_range.contains(left_range.end())
+        })
+        .count()
 }
-fn part_two(input: &str) -> u32 {
-    parse_input(input);
-
-    10
+fn part_two(input: &str) -> usize {
+    parse_input(input)
+        .iter()
+        .filter(|(left_range, right_range)| {
+            left_range.contains(right_range.start())
+                || left_range.contains(right_range.end())
+                || right_range.contains(left_range.start())
+                || right_range.contains(left_range.end())
+        })
+        .count()
 }
 
 pub fn main() {
     let input = include_str!("../inputs/04.txt");
 
-    println!("part one: {}", part_one(input)); // 7875
-    println!("part two: {}", part_two(input)); // 2479
+    println!("part one: {}", part_one(input)); // 524
+    println!("part two: {}", part_two(input)); // 798
 }
