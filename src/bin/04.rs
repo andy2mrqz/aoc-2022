f5043982fc0b8d1cd1def2ecd88e0be577b0fb2c
Andrew Marquez <andy2mgcc@gmail.com>
2022-12-04 00:40:36 -0700

    Apply filter once instead of map + filter

--- src/bin/04.rs
+++ src/bin/04.rs
@@ -5,7 +5,7 @@ type Set = HashSet<u32>;
 fn count_overlaps_where(input: &str, overlap_fn: &dyn Fn(&Set, &Set) -> bool) -> usize {
     input
         .lines()
-        .map(|assignments| {
+        .filter(|assignments| {
             let assignments: Vec<&str> = assignments.split(",").collect();
 
             let left_range: Vec<u32> = assignments[0].split("-").flat_map(|n| n.parse()).collect();
@@ -14,9 +14,8 @@ fn count_overlaps_where(input: &str, overlap_fn: &dyn Fn(&Set, &Set) -> bool) ->
             let right_range: Vec<u32> = assignments[1].split("-").flat_map(|n| n.parse()).collect();
             let right_set: HashSet<u32> = (right_range[0]..=right_range[1]).collect();
 
-            (left_set, right_set)
+            overlap_fn(&left_set, &right_set) || overlap_fn(&right_set, &left_set)
         })
-        .filter(|(set_a, set_b)| overlap_fn(set_a, set_b) || overlap_fn(set_b, set_a))
         .count()
 }
 
