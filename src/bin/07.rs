03069177f1b999cdca03ace91a88217c2601a5d9
Andrew Marquez <andy2mgcc@gmail.com>
2022-12-07 16:55:38 -0700

    Remove unnecessary code; simplify

--- src/bin/07.rs
+++ src/bin/07.rs
@@ -5,31 +5,25 @@ fn abs_path(path: &Vec<String>) -> String {
     format!("/{}", path.iter().skip(1).join("/"))
 }
 
-fn dir_size(dir: Vec<(usize, &str)>) -> usize {
-    dir.iter().fold(0, |acc, (size, _)| acc + size)
+fn dir_size(dir: Vec<usize>) -> usize {
+    dir.iter().sum()
 }
 
 fn get_sums(input: &str) -> HashMap<String, usize> {
     let mut path = Vec::new();
-    let mut stats: HashMap<String, Vec<(usize, &str)>> = HashMap::new();
+    let mut stats: HashMap<String, Vec<usize>> = HashMap::new();
 
     for line in input.lines() {
         if line == "$ cd .." {
             path.pop();
         } else if line.starts_with("$ cd ") {
-            let dir_name = line.split("$ cd ").collect::<Vec<&str>>()[1].to_owned();
+            let dir_name = line.split_once("$ cd ").unwrap().1.to_owned();
             path.push(dir_name);
             stats.insert(abs_path(&path), Vec::new());
-        } else if line.starts_with("dir") {
-            let dir_name = line.split("dir ").collect::<Vec<&str>>()[1].to_owned();
-            path.push(dir_name);
-            stats.insert(abs_path(&path), Vec::new());
-            path.pop();
         } else if line.starts_with(|c: char| c.is_numeric()) {
-            let (size, name) = line.split(" ").collect_tuple().unwrap();
-            let size: usize = size.parse().unwrap();
+            let size = line.split_once(" ").unwrap().0.parse().unwrap();
             let items = stats.get_mut(&abs_path(&path)).unwrap();
-            items.push((size, name));
+            items.push(size);
         }
     }
 
@@ -51,16 +45,9 @@ fn get_sums(input: &str) -> HashMap<String, usize> {
 
 fn part_one(input: &str) -> usize {
     let sums = get_sums(input);
-
     let max = 100_000;
-    let filtered_paths = sums
-        .iter()
-        .filter(|(_, size)| size <= &&max)
-        .collect::<Vec<_>>();
 
-    filtered_paths
-        .iter()
-        .fold(0, |acc: usize, (_, size)| acc + **size)
+    sums.values().filter(|size| size <= &&max).sum()
 }
 
 fn part_two(input: &str) -> usize {
@@ -72,19 +59,16 @@ fn part_two(input: &str) -> usize {
     let desired_space = 30_000_000;
     let amount_to_free = desired_space - unused_space;
 
-    let filtered_paths = sums
-        .iter()
-        .filter(|(_, size)| size >= &&amount_to_free)
-        .collect::<Vec<_>>();
-
-    let sizes: Vec<usize> = filtered_paths.iter().map(|(_, size)| **size).collect();
-
-    *sizes.iter().min().unwrap()
+    *sums
+        .values()
+        .filter(|&size| size >= &amount_to_free)
+        .min()
+        .unwrap()
 }
 
 pub fn main() {
     let input = include_str!("../inputs/07.txt");
 
     println!("part one: {}", part_one(input)); // 1315285
-    println!("part two: {}", part_two(input)); //
+    println!("part two: {}", part_two(input)); // 9847279
 }
