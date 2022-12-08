15de6d009e0d3a8516838000dfd416e73d0294d2
Andrew Marquez <andy2mgcc@gmail.com>
2022-12-08 12:19:08 -0700

    Solve day 08 part 2

--- src/bin/08.rs
+++ src/bin/08.rs
@@ -1,43 +1,58 @@
-use std::cmp::Ordering;
+fn visible_treeline(tree: char, trees: &Vec<char>) -> Vec<char> {
+    let mut taken = vec![];
+    for other in trees {
+        if tree.cmp(&other).is_le() {
+            taken.push(*other);
+            break;
+        } else {
+            taken.push(*other);
+        }
+    }
+    taken
+}
 
-fn solve(input: &str) -> usize {
-    let w = input.split_once("\n").unwrap().0.len();
-    let h = input.lines().count();
-
+fn solve(input: &str) -> (usize, usize) {
     let grid: Vec<Vec<char>> = input.lines().map(|row| row.chars().collect()).collect();
+    let w = grid.first().unwrap().len();
+    let h = grid.len();
 
-    let mut sum = 0;
+    let mut visible_trees = 0;
+    let mut scenic_score = 0;
     for r in 0..w {
         for c in 0..h {
             let tree = grid[r][c];
 
-            let left: Vec<char> = (0..c).map(|next| grid[r][next]).collect();
-            let right: Vec<char> = ((c + 1)..w).map(|next| grid[r][next]).collect();
-            let up: Vec<char> = (0..r).map(|next| grid[next][c]).collect();
-            let down: Vec<char> = ((r + 1)..h).map(|next| grid[next][c]).collect();
+            let left = (0..c).rev().map(|next| grid[r][next]).collect();
+            let right = ((c + 1)..w).map(|next| grid[r][next]).collect();
+            let up = (0..r).rev().map(|next| grid[next][c]).collect();
+            let down = ((r + 1)..h).map(|next| grid[next][c]).collect();
+            let neighbors: [Vec<char>; 4] = [left, right, up, down];
 
-            if left
-                .iter()
-                .all(|other| tree.cmp(other) == Ordering::Greater)
-                || right
-                    .iter()
-                    .all(|other| tree.cmp(other) == Ordering::Greater)
-                || up.iter().all(|other| tree.cmp(other) == Ordering::Greater)
-                || down
-                    .iter()
-                    .all(|other| tree.cmp(other) == Ordering::Greater)
-            {
-                sum += 1
+            // visible trees logic
+            let is_visible = neighbors.iter().fold(false, |acc, treeline| {
+                acc || treeline.iter().all(|other| tree.cmp(&other).is_gt())
+            });
+            if is_visible {
+                visible_trees += 1;
+            }
+            // scenic score logic
+            let total_score = neighbors.iter().fold(1, |acc, treeline| {
+                acc * visible_treeline(tree, treeline).len()
+            });
+
+            if total_score > scenic_score {
+                scenic_score = total_score
             }
         }
     }
 
-    sum
+    (visible_trees, scenic_score)
 }
 
 pub fn main() {
     let input = include_str!("../inputs/08.txt");
+    let answer = solve(input);
 
-    println!("part one: {}", solve(input)); // 1688
-    // println!("part two: {}", solve(input)); //
+    println!("part one: {}", answer.0); // 1688
+    println!("part two: {}", answer.1); // 410400
 }
