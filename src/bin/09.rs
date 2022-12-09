c488aafef6755a0816c78c1a48a8f093c03fbda5
Andrew Marquez <andy2mgcc@gmail.com>
2022-12-09 00:17:51 -0700

    Solve day 09 part 2

--- src/bin/09.rs
+++ src/bin/09.rs
@@ -6,66 +6,60 @@ struct Coord {
     y: i32,
 }
 
-fn two_steps_apart(head: Coord, tail: Coord) -> bool {
+fn two_steps_apart(head: &Coord, tail: &Coord) -> bool {
     (head.y - tail.y >= 2)
         || (tail.y - head.y >= 2)
         || (head.x - tail.x >= 2)
         || (tail.x - head.x >= 2)
 }
 
-fn diagonal(head: Coord, tail: Coord) -> bool {
+fn diagonal(head: &Coord, tail: &Coord) -> bool {
     (head != tail) && two_steps_apart(head, tail)
 }
 
-fn solve(instructions: &Vec<(char, u32)>) -> usize {
-    let mut head = Coord { x: 0, y: 0 };
-    let mut tail = Coord { x: 0, y: 0 };
-
+fn solve(instructions: &Vec<(char, u32)>, mut knots: Vec<Coord>) -> usize {
     let mut visited: HashSet<_> = HashSet::new();
-    visited.insert(tail);
+    visited.insert(knots[0]);
 
     for (dir, steps) in instructions {
         for _ in 0..*steps {
+            let head = knots.get_mut(0).unwrap();
             match dir {
-                'R' => {
-                    head.x += 1;
-                    if two_steps_apart(head, tail) && head.y == tail.y {
-                        tail.x += 1;
-                    } else if diagonal(head, tail) {
-                        tail.x += 1;
-                        tail.y += if head.y > tail.y { 1 } else { -1 }
-                    }
-                }
-                'L' => {
-                    head.x -= 1;
-                    if two_steps_apart(head, tail) && head.y == tail.y {
-                        tail.x -= 1;
-                    } else if diagonal(head, tail) {
-                        tail.x -= 1;
-                        tail.y += if head.y > tail.y { 1 } else { -1 }
-                    }
-                }
-                'U' => {
-                    head.y += 1;
-                    if two_steps_apart(head, tail) && head.x == tail.x {
-                        tail.y += 1;
-                    } else if diagonal(head, tail) {
-                        tail.y += 1;
-                        tail.x += if head.x > tail.x { 1 } else { -1 }
-                    }
-                }
-                'D' => {
-                    head.y -= 1;
-                    if two_steps_apart(head, tail) && head.x == tail.x {
-                        tail.y -= 1;
-                    } else if diagonal(head, tail) {
-                        tail.y -= 1;
-                        tail.x += if head.x > tail.x { 1 } else { -1 }
-                    }
-                }
+                'R' => head.x += 1,
+                'L' => head.x -= 1,
+                'U' => head.y += 1,
+                'D' => head.y -= 1,
                 _ => {}
             }
-            visited.insert(tail);
+            for idx in 1..knots.len() {
+                let next = knots[idx - 1];
+                let curr = knots.get_mut(idx).unwrap();
+                if two_steps_apart(&next, curr) && (next.x == curr.x || next.y == curr.y) {
+                    if next.x == curr.x {
+                        curr.y += if next.y > curr.y { 1 } else { -1 }
+                    } else {
+                        curr.x += if next.x > curr.x { 1 } else { -1 }
+                    }
+                } else if diagonal(&next, curr) {
+                    if next.x > curr.x && next.y > curr.y {
+                        curr.x += 1;
+                        curr.y += 1;
+                    } else if next.x > curr.x && next.y < curr.y {
+                        curr.x += 1;
+                        curr.y -= 1;
+                    } else if next.x < curr.x && next.y > curr.y {
+                        curr.x -= 1;
+                        curr.y += 1;
+                    } else if next.x < curr.x && next.y < curr.y {
+                        curr.x -= 1;
+                        curr.y -= 1;
+                    }
+                }
+                if idx == knots.len() - 1 {
+                    let tail = *knots.get(knots.len() - 1).unwrap();
+                    visited.insert(tail);
+                }
+            }
         }
     }
     visited.len()
@@ -82,6 +76,8 @@ pub fn main() {
         })
         .collect();
 
-    println!("part one: {}", solve(&instructions)); // 5619
-    println!("part two: {}", solve(&instructions)); //
+    let starting_knot = Coord { x: 0, y: 0 };
+
+    println!("part one: {}", solve(&instructions, vec![starting_knot; 2])); // 5619
+    println!("part two: {}", solve(&instructions, vec![starting_knot; 10])); // 2376
 }
