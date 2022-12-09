9b9db5483d6afb8505eb379eb615247cfbf93fce
Andrew Marquez <andy2mgcc@gmail.com>
2022-12-09 00:50:10 -0700

    Simplify diagonal logic; track visits more easily

--- src/bin/09.rs
+++ src/bin/09.rs
@@ -32,24 +32,19 @@ fn solve(instructions: &Vec<(char, u32)>, mut knots: Vec<Coord>) -> (usize, usiz
             for idx in 1..knots.len() {
                 let next = knots[idx - 1];
                 let curr = knots.get_mut(idx).unwrap();
-                if two_steps_apart(&next, curr) && (next.x == curr.x || next.y == curr.y) {
+                if two_steps_apart(&next, curr) {
                     if next.x == curr.x {
                         curr.y += if next.y > curr.y { 1 } else { -1 }
-                    } else {
+                    } else if next.y == curr.y {
                         curr.x += if next.x > curr.x { 1 } else { -1 }
+                    } else if &next != curr {
+                        curr.x += if next.x > curr.x { 1 } else { -1 };
+                        curr.y += if next.y > curr.y { 1 } else { -1 };
                     }
-                } else if two_steps_apart(&next, curr) && &next != curr {
-                    curr.x += if next.x > curr.x { 1 } else { -1 };
-                    curr.y += if next.y > curr.y { 1 } else { -1 };
-                }
-                if idx == knots.len() - 1 {
-                    let tail = *knots.get(knots.len() - 1).unwrap();
-                    visited_part_two.insert(tail);
-                } else if idx == 1 {
-                    let tail = *knots.get(1).unwrap();
-                    visited_part_one.insert(tail);
                 }
             }
+            visited_part_one.insert(*knots.get(1).unwrap());
+            visited_part_two.insert(*knots.get(9).unwrap());
         }
     }
     (visited_part_one.len(), visited_part_two.len())
