44f7304d05777831987c50a9f029c99733500a9c
Andrew Marquez <andy2mgcc@gmail.com>
2022-12-10 23:53:11 -0700

    Solve day 10 part 2

--- src/bin/10.rs
+++ src/bin/10.rs
@@ -1,15 +1,4 @@
-fn solve(input: &str) -> i32 {
-    let instructions: Vec<(i32, i32)> = input
-        .lines()
-        .map(|line| {
-            if line == "noop" {
-                (1, 0)
-            } else {
-                (2, line[5..].parse().unwrap())
-            }
-        })
-        .collect();
-
+fn part_one(instructions: Vec<(usize, i32)>) -> i32 {
     let mut signal_strength = Vec::new();
     let mut x = 1;
     let mut cycle: i32 = 1;
@@ -29,10 +18,42 @@ fn solve(input: &str) -> i32 {
     signal_strength.iter().sum()
 }
 
+fn part_two(instructions: Vec<(usize, i32)>) -> () {
+    let mut x: i32 = 1;
+    let mut cycle: i32 = 1;
+    let mut crt = vec!['.'; 240];
+
+    for (cost, value) in instructions {
+        for i in 1..=cost {
+            if ((x - 1)..=(x + 1)).contains(&(cycle - 1).rem_euclid(40)) {
+                crt[cycle as usize - 1] = '#';
+            }
+            cycle += 1;
+            if i == cost {
+                x += value;
+            }
+        }
+    }
+
+    for line in crt.chunks(40) {
+        println!("{:?}", String::from_iter(line))
+    }
+}
+
 pub fn main() {
     let input = include_str!("../inputs/10.txt");
-    let answer = solve(input);
+    let instructions: Vec<(usize, i32)> = input
+        .lines()
+        .map(|line| {
+            if line == "noop" {
+                (1, 0)
+            } else {
+                (2, line[5..].parse().unwrap())
+            }
+        })
+        .collect();
 
-    println!("part one: {}", answer); // 13820
-    println!("part two: {}", answer); //
+    println!("part one: {}", part_one(instructions.clone())); // 13820
+    println!("part two");
+    part_two(instructions); // ZKGRKGRK
 }
